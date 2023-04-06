2e2aa4ba5159869caed9800bdb4bcb84889a7919
Brian <bstanton773@gmail.com>
2023-04-05 19:29:47 -0500

    Initial Commit with everything

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.blueprints.api import api
+app.register_blueprint(api)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
